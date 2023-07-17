6db64fce879c29d70d6fc226914e6efe2b38dfcd
anishmitagar <Anish.S.Mitagar@gmail.com>
2023-07-17 16:43:02 -0400

    added files to setup db

---
+++ DB_Setup/setup.sh
@@ -0,0 +1,18 @@
+echo Creating Single Cockroach Node...
+
+cockroach start --insecure --store=node1 --listen-addr=localhost:26257 --http-addr=localhost:8080 --join=localhost:26257,localhost:26258,localhost:26259 --background
+
+echo Initializing Single Cockroach Node Cluster...
+
+cockroach init --insecure --host=localhost:26257
+
+grep 'node starting' node1/logs/cockroach.log -A 11
+
+echo Running sql script...
+
+cockroach sql --insecure --host=localhost:26257 --file setup.sql
+
+echo Finished Setting Up Database, now connecting to SQL shell...
+
+cockroach sql --insecure --host=localhost:26257
+
